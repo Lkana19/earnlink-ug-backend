8ffe725ca684add3cb58b58d059e7989c064d781
Lkana19 <142789934+Lkana19@users.noreply.github.com>
2026-09-22 11:51:14 +0300

    add frontened integration

--- server.js
+++ server.js
@@ -14,10 +14,12 @@
 
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public'))); // serves your index.html
 
 const PESAPAL_BASE = 'https://pay.pesapal.com/v3'; // use https://cybqa.pesapal.com/pesapalv3 for sandbox/testing
 
